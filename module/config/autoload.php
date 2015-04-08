d1fced56818f05b285644e64c961132a74b371ac
David Molineus <david.molineus@netzmacht.de>
2015-04-08 06:48:05 +0200

    Uncomment manual installation autoloading support.

--- module/config/autoload.php
+++ module/config/autoload.php
@@ -10,12 +10,12 @@
  */
 
 // Uncomment if install it manually.
-ClassLoader::addClasses(
-    array(
-        'Netzmacht\Contao\CacheControl\Infrastructure\Base'      => 'system/modules/cache-control/classes/src/Infrastructure/Base.php',
-        'Netzmacht\Contao\CacheControl\Infrastructure\DcaHelper' => 'system/modules/cache-control/classes/src/Infrastructure/DcaHelper.php',
-        'Netzmacht\Contao\CacheControl\Infrastructure\Hooks'     => 'system/modules/cache-control/classes/src/Infrastructure/Hooks.php',
-        'Netzmacht\Contao\CacheControl\PageCache'                => 'system/modules/cache-control/classes/src/PageCache.php',
-        'Netzmacht\Contao\CacheControl\Query'                    => 'system/modules/cache-control/classes/src/Query.php',
-    )
-);
+//ClassLoader::addClasses(
+//    array(
+//        'Netzmacht\Contao\CacheControl\Infrastructure\Base'      => 'system/modules/cache-control/classes/src/Infrastructure/Base.php',
+//        'Netzmacht\Contao\CacheControl\Infrastructure\DcaHelper' => 'system/modules/cache-control/classes/src/Infrastructure/DcaHelper.php',
+//        'Netzmacht\Contao\CacheControl\Infrastructure\Hooks'     => 'system/modules/cache-control/classes/src/Infrastructure/Hooks.php',
+//        'Netzmacht\Contao\CacheControl\PageCache'                => 'system/modules/cache-control/classes/src/PageCache.php',
+//        'Netzmacht\Contao\CacheControl\Query'                    => 'system/modules/cache-control/classes/src/Query.php',
+//    )
+//);
