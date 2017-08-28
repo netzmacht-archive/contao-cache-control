979d1c065c26847dcc25bd6aa3c1c44b0cac2688
David Molineus <david.molineus@netzmacht.de>
2017-08-28 15:22:41 +0200

    Move icon to bundle namespace.

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -22,7 +22,7 @@ $GLOBALS['TL_DCA']['tl_page']['select']['buttons_callback'][] = array(
 $GLOBALS['TL_DCA']['tl_page']['list']['operations']['clearCache'] = array
 (
     'label'           => &$GLOBALS['TL_LANG']['tl_page']['clearCache'],
-    'icon'            => 'system/modules/cache-control/assets/clear-cache.png',
+    'icon'            => 'bundles/netzmachtcontaocachecontrol/clear-cache.png',
     'href'            => 'act=show&amp;clearCache=1',
     'button_callback' => array('Netzmacht\Contao\CacheControl\Infrastructure\DcaHelper', 'generateButton'),
 );
