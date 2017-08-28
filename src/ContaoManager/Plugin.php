e01f4e3cc8fe00c378d3e29f9f57ee6edbe305ef
David Molineus <david.molineus@netzmacht.de>
2017-08-28 15:23:54 +0200

    Make it an contao bundle.

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @package    contao-cache-control
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2017 netzmacht David Molineus. All rights reserved.
+ * @filesource
+ *
+ */
+
+namespace Netzmacht\Contao\CacheControl\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use FOS\HttpCacheBundle\FOSHttpCacheBundle;
+use Netzmacht\Contao\CacheControl\NetzmachtContaoCacheControlBundle;
+
+/**
+ * Class Plugin
+ *
+ * @package Netzmacht\Contao\CacheControl\ContaoManager
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(FOSHttpCacheBundle::class),
+            BundleConfig::create(NetzmachtContaoCacheControlBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class])
+                ->setLoadAfter([FOSHttpCacheBundle::class])
+        ];
+    }
+}
---
+++ src/NetzmachtContaoCacheControlBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @package    contao-cache-control
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2017 netzmacht David Molineus. All rights reserved.
+ * @filesource
+ *
+ */
+
+namespace Netzmacht\Contao\CacheControl;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class NetzmachtContaoCacheControlBundle
+ *
+ * @package Netzmacht\Contao\CacheControl
+ */
+class NetzmachtContaoCacheControlBundle extends Bundle
+{
+}
